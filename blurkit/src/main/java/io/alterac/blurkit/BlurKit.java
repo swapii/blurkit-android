011a9d995e47c50fea9229d1cbe4fe354d9cd9b2
Pavel Savinov <swapii@gmail.com>
2020-09-10 14:26:40 +0300

    Use support classes fom RenderScript

--- blurkit/src/main/java/io/alterac/blurkit/BlurKit.java
+++ blurkit/src/main/java/io/alterac/blurkit/BlurKit.java
@@ -5,10 +5,10 @@ import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Matrix;
 import android.view.View;
-import android.renderscript.Allocation;
-import android.renderscript.Element;
-import android.renderscript.RenderScript;
-import android.renderscript.ScriptIntrinsicBlur;
+import android.support.v8.renderscript.Allocation;
+import android.support.v8.renderscript.Element;
+import android.support.v8.renderscript.RenderScript;
+import android.support.v8.renderscript.ScriptIntrinsicBlur;
 
 public class BlurKit {
 
